e5918bd2c2d953654b4ee25087c65f8f8dfffa42
timorpheus <tymurtolochko@gmail.com>
2018-05-28 22:04:13 +0300

    init

---
+++ app/mail.php
@@ -0,0 +1,41 @@
+<?php
+$to = "tt.softevol@gmail.com";
+$subject = "HTML email";
+
+  $visitor_email = $_POST['visitor-email'];
+
+
+$message = " 
+<html>
+<head>
+<title>HTML email</title>
+</head>
+<body>
+<p>This email contains HTML Tags!</p>
+<table>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+</tr>
+<tr>
+<td>John</td>
+<td>Doe</td>
+</tr>
+</table>
+
+" . $visitor_email.
+"
+</body>
+</html>
+";
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// More headers
+$headers .= 'From: <webmaster@example.com>' . "\r\n";
+$headers .= 'Cc: myboss@example.com' . "\r\n";
+
+mail($to,$subject,$message,$headers);
+?>
